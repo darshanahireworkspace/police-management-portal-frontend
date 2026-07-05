9756a5b5d6bb50a05a8fcf1450dadc49ea7ec3b8
Darshan Ahire <ahiredarshan399@gmail.com>
2026-07-05 15:52:37 +0530

    Added PWA icons

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,47 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Chhavani Police Station Malegaon",
+        short_name: "Chhavani Police",
+        description: "Police City Religious and Festival Management System",
+        theme_color: "#0b1f3a",
+        background_color: "#eef4fb",
+        display: "standalone",
+        orientation: "portrait",
+        start_url: "/",
+        scope: "/",
+        icons: [
+  {
+    src: "/icon-192.png",
+    sizes: "192x192",
+    type: "image/png",
+  },
+  {
+    src: "/icon-512.png",
+    sizes: "512x512",
+    type: "image/png",
+  },
+]
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/.*\.up\.railway\.app\/api\/.*/i,
+            handler: "NetworkFirst",
+            options: {
+              cacheName: "api-cache",
+              networkTimeoutSeconds: 5,
+            },
+          },
+        ],
+      },
+    }),
+  ],
+});
