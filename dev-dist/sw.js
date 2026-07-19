13de63bb1fd2f4b28f611098ccbd45302bc2d4a8
Darshan Ahire <ahiredarshan033@gmail.com>
2026-07-19 19:41:58 +0530

    Fix iPhone 13 Pro PWA layout and safe area

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-970124e6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.an5v899kp9"
+    "revision": "0.u1r1iv4f7k8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
